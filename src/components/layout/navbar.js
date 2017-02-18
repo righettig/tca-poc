5729628576bfab098d5b76e402bbc9297ba32cb7
grighetti <grighetti@britech.com.br>
2017-02-18 20:01:52 +0000

    Improved visuals for the navbar components

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,13 +1,6 @@
 app.component("navbar", {
-    template: `
-        <div style='background: black; color: white; padding: 4px'>
-            <a ui-sref='auth.analytics'>PreTrade</a>
-            <a ui-sref='auth.monitor'>Monitor</a>
-            <a ui-sref='auth.about'>About</a>
-
-            <a ng-click='$ctrl.logout()'>Logout</>
-        </div>
-    `,
+    templateUrl: "src/components/layout/navbar.html",
+    
     controller: function(AuthService, $state) {
         this.logout = function() {
             AuthService.logout();
