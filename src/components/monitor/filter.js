418f27af81d10ad0deb713036d2bc78fe11cac9a
grighetti <grighetti@britech.com.br>
2017-02-19 17:24:03 +0000

    Improved UI for the other filters
    Created dropdown component

--- src/components/monitor/filter.js
+++ src/components/monitor/filter.js
@@ -4,6 +4,23 @@ app.component("filter", {
         onFilterChanged: "&"
     },
     controller: function(DTA) {
-        this.clients = ["c1", "c2"]
+        this.clients = ["CLN0001", "CLN0002"]
+        this.symbols = ["AAPL", "MSFT"]
+        this.baskets = ["BSK0001"]
+        this.desks = ["DSK0001"]
+    }
+});
+
+app.component("dropdown", {
+    template: `
+        <select ng-model="selectedItem" 
+                ng-options="item for item in $ctrl.items" 
+                ng-change="$ctrl.onSelected({item:selectedItem})"></select>
+
+        <button ng-click="$ctrl.onSelected({item:null}); selectedItem=null">x</button>
+    `,
+    bindings: {
+        items: "<",
+        onSelected: "&"
     }
 });
