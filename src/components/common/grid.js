ada7f14f62a972a5f27fbf28bc3a046f94763ba3
grighetti <grighetti@britech.com.br>
2017-02-19 16:30:19 +0000

    Added moreRows functionality
    The grid component gets assigned a unique id

--- src/components/common/grid.js
+++ src/components/common/grid.js
@@ -6,13 +6,37 @@ app.component('genesisGrid', {
         name: "@",
         options: "<"
     },
-    controller: function() {
+    controller: function($element) {
         this.$onInit = () => {
+            this.id = this.createGuid();
+            $element.attr("id", this.id)
+            
             this.options.onGridReady = event => {
                 this.mandatoryFields = 
                     this.options.columnDefs
                         .filter(c => c.isMandatory)
                         .map(c => c.field);
+                
+                var viewport = $("#" + this.id + " .ag-body-viewport");
+                var gridHeight = $("#" + this.id + " .ag-body-container");
+                var prevLeft = 0; // we want to ignore horizontal scrolls
+                
+                viewport.scroll(
+                    this.throttle(() => {
+                        var currentLeft = viewport.scrollLeft();
+                        if (prevLeft !== currentLeft) {
+                            prevLeft = currentLeft;
+                            return;
+                        }
+                        var threshold = 
+                            gridHeight.height() - (viewport.height() + viewport.scrollTop());
+                        
+                        console.log(threshold);
+                        if (threshold <= 400) {
+                            //this.options.api.showLoadingOverlay();
+                            console.log("MORE_ROWS");
+                        }
+                    }, 400));
                 
                 this.loadData();
             }
@@ -38,6 +62,26 @@ app.component('genesisGrid', {
         }
         
         this.mandatoryFields = []; 
+        
+        this.throttle = function(callback, limit) {
+                var wait = false;               // Initially, we're not waiting
+                return function() {             // We return a throttled function
+                    if (!wait) {                // If we're not waiting
+                        callback.call();        // Execute users function
+                        wait = true;            // Prevent future invocations
+                        setTimeout(function() { // After a period of time
+                            wait = false;       // And allow future invocations
+                        }, limit);
+                    }
+                }
+            }
+        
+        this.createGuid = 
+            () =>
+                'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {  
+                    var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);  
+                    return v.toString(16);  
+                });
         
         // a,b,c + a,d=> a,b,c,d
         this.merge = 
