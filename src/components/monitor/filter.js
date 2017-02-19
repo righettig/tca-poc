2d339a31edaaf1def79c872b7e0d7852fbd123b4
grighetti <grighetti@britech.com.br>
2017-02-19 17:10:31 +0000

    Improved dropdown for "client" filter

--- src/components/monitor/filter.js
+++ src/components/monitor/filter.js
@@ -2,5 +2,8 @@ app.component("filter", {
     templateUrl: 'src/components/monitor/filter.html',
     bindings: {
         onFilterChanged: "&"
+    },
+    controller: function(DTA) {
+        this.clients = ["c1", "c2"]
     }
 });
